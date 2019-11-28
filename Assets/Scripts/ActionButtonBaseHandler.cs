dee065537c2897060f3639822fa0e9e39494d978
AndriiZozulia <paptor63@gmail.com>
2019-11-28 18:15:44 +0200

    додано сцени: Settings, Menu, Gamefield реалізовано переходи між сценами, базову поведінку UI елементів

---
+++ Assets/Scripts/ActionButtonBaseHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionButtonBaseHandler : MonoBehaviour
+{
+    private SceneLoader _sceneLoader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sceneLoader = GetComponent<SceneLoader>();    
+    }
+
+    virtual public void Action()
+    {
+        //Must be overrided
+    }
+
+    private void OnMouseDown()
+    {
+        // Do some action and load scene
+        Action();
+        if (_sceneLoader)
+        {
+            _sceneLoader.LoadScene();
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public string sceneName;
+
+    public void LoadScene()
+    {
+        if (Application.CanStreamedLevelBeLoaded(sceneName))
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+        else
+        {
+            Debug.Log("Scene with name \"" + sceneName + "\" is not exist");
+        }
+    }
+}
