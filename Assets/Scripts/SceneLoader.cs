ba79ffe954a9cdc8fb121d0a86e9fc1d7ca9d2de
AndriiZozulia <paptor63@gmail.com>
2019-12-15 12:19:41 +0200

    Правка сцен і обслуговуючих скриптів

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using Entity;
 
 public class SceneLoader : MonoBehaviour
 {
@@ -8,13 +9,41 @@ public class SceneLoader : MonoBehaviour
 
     public void LoadScene()
     {
-        if (Application.CanStreamedLevelBeLoaded(sceneName))
+        if (sceneName.Equals(""))
         {
+            sceneName = XMLUtil.Deserialize<SceneEntity>("Assets/base_mm/LastLoadedScene.xml").name;
+        }
+
+        SaveLastScene();
+
+        if (Application.CanStreamedLevelBeLoaded(sceneName))
+        { 
             SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
         }
         else
         {
             Debug.Log("Scene with name \"" + sceneName + "\" is not exist");
         }
+    }
+
+    private void SaveLastScene()
+    {
+        SceneEntity sceneItem = new SceneEntity();
+        sceneItem.name = SceneManager.GetActiveScene().name;
+
+        XMLUtil.Serialize(sceneItem, "Assets/base_mm/LastLoadedScene.xml");
+
+        /*QuestManager questManager = new QuestManager();
+        QuestEntity quest = new QuestEntity();
+        DialogEntity dialog = new DialogEntity();
+        dialog.text = "text1";
+        quest.dialog = new DialogEntity[1];
+        quest.dialog[0] = dialog;
+        quest.id = "Chapter1/Day1/Ch1_Day1_POSOSAT_HUY";
+        questManager.quests = new string[1];
+        questManager.quests[0] = quest.id;
+
+        XMLUtil.Serialize(quest, "Assets/base_mm/GameStory/" + quest.id + ".xml");
+        XMLUtil.Serialize(questManager, "Assets/base_mm/Quests.xml");*/
     }
 }
