860a224db9bdcb994b347679fe57d17cf70e1d6f
AndriiZozulia <paptor63@gmail.com>
2019-12-20 05:56:20 +0200

    Save reset

---
+++ Assets/Scripts/Actions/ActionButtonSave.cs
@@ -0,0 +1,12 @@
+﻿
+public class ActionButtonSave : ActionButtonBaseHandler
+{
+    public ResetProgress reset;
+    public override void Action()
+    {
+        if (reset.Reseted())
+        {
+            SaveManager.GetInstance().CreatePlayerSave();
+        }
+    }
+}
---
+++ Assets/Scripts/ResetProgress.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetProgress : MonoBehaviour
+{
+    public Text intelligence;
+    public Text force;
+
+    private bool reseted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reseted = false;
+        var rating = SaveManager.GetInstance().GetRating();
+        intelligence.text = rating.intelligence.ToString();
+        force.text = rating.force.ToString();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        reseted = true;
+        intelligence.text = "0";
+        force.text = "0";
+    }
+
+    public bool Reseted()
+    {
+        return reseted;
+    }
+}
