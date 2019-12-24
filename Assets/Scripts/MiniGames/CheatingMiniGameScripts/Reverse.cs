97f82c9638097ba5c18f29b13eb1fba141713eef
Andrey Lisogor <lisogor.andrey7@ukr.net>
2019-12-24 20:26:26 +0200

    MiniGames Fixes

--- Assets/Scripts/MiniGames/CheatingMiniGameScripts/Reverse.cs
+++ Assets/Scripts/MiniGames/CheatingMiniGameScripts/Reverse.cs
@@ -1,43 +1,57 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class Reverse : MiniGameAction
+public class Reverse : MiniGameAction
 {
-  public static bool isFinished = false;
-  public static int score = 0;
- 
-    public void CheatOnTeacher(){
-     var slide =  ProgressSlider.slider.GetComponent<Slider>();
-     var hSlide = HealthSlider.HSlider.GetComponent<Slider>();
-     if(slide.value < 20){
-       if (hSlide.value > 0) {
-              if(TestIndicator.isWatching == true) {
-                    slide.value++;
+    public static bool isFinished = false;
+    public static int score = 0;
+
+    public void CheatOnTeacher()
+    {
+        var slide = ProgressSlider.slider.GetComponent<Slider>();
+        var hSlide = HealthSlider.HSlider.GetComponent<Slider>();
+        if (slide.value < 20)
+        {
+            if (hSlide.value > 0)
+            {
+                if (TestIndicator.isWatching == true)
+                {
+                    slide.value++;
                     score++;  //Score number adding
-              } else {
-                    hSlide.value--;
-              }
-              } else if(hSlide.value <= 0){
-                  isFinished = true;
                 }
-     } else if(slide.value == 20){
-        isFinished = true;
-     }
-    }
-
-    public static int gameFinished(){
-      Time.timeScale = 0;
-        return score;
-      
+                else
+                {
+                    hSlide.value--;
+                }
+            }
+            else if (hSlide.value <= 0)
+            {
+                isFinished = true;
+            }
+        }
+        else if (slide.value == 20)
+        {
+            isFinished = true;
+        }
     }
-    public new void  Start(){
-      base.Start();
 
-}
-    void Update(){
-      if(isFinished == true){
-        OnFinish(new RatingEntity());
-      }
+    public static int gameFinished()
+    {
+        Time.timeScale = 0;
+        return score;
+
+    }
+    public new void Start()
+    {
+        base.Start();
+
+    }
+    void Update()
+    {
+        if (isFinished == true)
+        {
+            OnFinish(new RatingEntity());
+        }
     }
 }
 
--- Assets/Scripts/MiniGames/GymMiniGameScripts/Circles.cs
+++ Assets/Scripts/MiniGames/GymMiniGameScripts/Circles.cs
@@ -3,53 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Circles : MiniGameAction
+public class Circles : MiniGameAction
 {
-    public GameObject firstCrcl;
-    public GameObject secondCrcl;
-    public GameObject thirdCrcl;
-    public GameObject fourthCrcl;
-    public static GameObject [] MarkedCircles = new GameObject[4];
+    public GameObject firstCrcl;
+    public GameObject secondCrcl;
+    public GameObject thirdCrcl;
+    public GameObject fourthCrcl;
+    public static GameObject[] MarkedCircles = new GameObject[4];
 
     public static int index = 0;
     public static bool bluePrinted = false;
     public static int BlueCircles = 0;
 
- public new void Start()
-    { 
+    public new void Start()
+    {
         base.Start();
         MarkedCircles[0] = firstCrcl;
         MarkedCircles[1] = secondCrcl;
         MarkedCircles[2] = thirdCrcl;
         MarkedCircles[3] = fourthCrcl;
-    }
+    }
 
-   
-    void Update(){
-        if(!ReverseTimer.isFinished){
+
+    void Update()
+    {
+        if (!ReverseTimer.isFinished)
+        {
             CheckIndex();
         }
     }
 
-    public void CheckIndex(){
-        if(index == 4){
+    public void CheckIndex()
+    {
+        if (index == 4)
+        {
             ReverseTimer.isFinished = true;
             GetResult();
         }
     }
-    public static void DrawBlue(){
+    public static void DrawBlue()
+    {
         MarkedCircles[index].GetComponent<Image>().color = Color.blue;
         index++;
         bluePrinted = true;
         BlueCircles++;
     }
 
-    public static void DrawRed(){
+    public static void DrawRed()
+    {
         MarkedCircles[index].GetComponent<Image>().color = Color.red;
         index++;
     }
 
-    public void GetResult(){
+    public void GetResult()
+    {
         OnFinish(new RatingEntity());
     }
 }
